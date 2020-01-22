2a26be990b675144b6f47dca0e7e4eafa8d52d48
Joe Chandler <josephalanchandlerjr@gmail.com>
2020-01-22 15:38:52 -0500

    add fixtures folder and mer user router tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -99,4 +99,33 @@ test('Should not delete account for unauthenticated user', async () => {
 		.expect(401)
 })
 
+test('Should upload an avatar image', async () => {
+	await request(app)
+		.post('/users/me/avatar')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.attach('avatar', 'tests/fixtures/profile-pic.jpg')	
+		.expect(200)
 
+	const user = await User.findById(userOneId)
+	expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			name: 'Figment'
+		})
+		.expect(200)
+})
+
+test('Should not update invalid user fields', async () => {
+	await request(app)
+		.patch('/users/me')
+		.set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+		.send({
+			location: 'philly'
+		})
+		.expect(400)
+})
